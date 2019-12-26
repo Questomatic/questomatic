a6f808c883b91a525fd0494950e0f6c1bedb45b9
Roman Savrulin <romansavrulin@gmail.com>
2019-12-26 13:32:38 +0300

    remove unused stuff and fix image generator

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -8,8 +8,8 @@ docker run --privileged linuxkit/binfmt:v0.6
 docker build -t questomatic:latest .
 
 docker run --privileged -it questomatic:latest /bin/sh -c 'chroot /sysroot /debootstrap/debootstrap --second-stage'
-docker commit `docker ps -l --format="{{.ID}}"` questomatic:latest-1
-docker run --privileged -it questomatic:latest-1 /bin/sh -c 'chroot /sysroot apt install \
+docker commit `docker ps -l --format="{{.ID}}"` questomatic:latest
+docker run --privileged -it questomatic:latest /bin/sh -c 'chroot /sysroot apt install \
 --allow-unauthenticated -y libc6-dev libjsoncpp-dev libboost-filesystem-dev libboost-regex-dev \
 libboost-python-dev libboost-thread-dev libboost-date-time-dev libboost-program-options-dev libssl-dev libz-dev && apt-get clean'
-docker commit `docker ps -l --format="{{.ID}}"` questomatic:latest-2
+docker commit `docker ps -l --format="{{.ID}}"` questomatic:latest
